c4595b266c1bf78c955e257f0054c4c42e14dd81
ryfazrin <riansera515@gmail.com>
2023-03-17 20:28:32 +0700

    added: api jobs

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
+const https = require('https');
+const axios = require('axios');
 const auth = require('./middleware/auth')
 const connection = require('./config/sql-database');
 const app = express();
@@ -25,17 +27,17 @@ app.post('/api/login', (req, res) => {
       console.log(user);
       if (password == user.password) {
         const token = jwt.sign({ userId: user.id }, 'my-secret-key', { expiresIn: '1h' });
-          connection.query('UPDATE users SET token = ? WHERE id = ?', [token, user.id], (error) => {
-            if (error) {
-              console.log('Error updating user token:', error);
-              res.sendStatus(500);
-            } else {
-              res.send({ token });
-              res.status().json({
-                token
-              })
-            }
-          });
+        connection.query('UPDATE users SET token = ? WHERE id = ?', [token, user.id], (error) => {
+          if (error) {
+            console.log('Error updating user token:', error);
+            res.sendStatus(500);
+          } else {
+            res.send({ token });
+            res.status().json({
+              token
+            })
+          }
+        });
       } else {
         res.status(401).send('Invalid username or password');
       }
@@ -75,6 +77,22 @@ app.post('/api/logout', auth.verifyToken, (req, res) => {
       res.sendStatus(200);
     }
   });
+});
+
+app.get('/api/jobs', async (req, res) => {
+  try {
+    const response = await axios.get('http://dev3.dansmultipro.co.id/api/recruitment/positions.json')
+    res.status(200).json(response.data);
+  }
+  catch (err) {
+    console.log(err);
+    
+    return res.json({
+      error: 1,
+      message: err.message,
+      fields: err.errors
+    });
+  }
 });
 
 // buat server nya
